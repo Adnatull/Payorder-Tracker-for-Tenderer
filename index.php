8039e4b707065f28f50444ebbe12ac1ca5bf43af
Adnatull <adnaan.masum@yahoo.com>
2018-05-19 00:42:14 +0600

    skeleton

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+    require_once("inc/db/db.php");
+    require_once("inc/header.php");
+    require_once("inc/engine/session.php");
+?>
+<h3>Hello World!</h3>
+
+<?php
+    require_once("inc/footer.php")
+?>
